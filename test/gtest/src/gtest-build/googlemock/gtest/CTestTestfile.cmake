5c8fbf1d7cc4f9d2f724a1b0575e4beca1432c5b
Kazu Otani <kazuotani14@gmail.com>
2017-04-03 03:05:19 +0200

    .

--- test/gtest/src/gtest-build/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/Kazu/Code/ilqr/test/gtest/src/gtest/googletest
-# Build directory: /Users/Kazu/Code/ilqr/test/gtest/src/gtest-build/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
